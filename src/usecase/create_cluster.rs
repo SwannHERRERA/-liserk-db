0b32621fc0b9aedf7a775de24f54ca2c168b0a44
Swann HERRERA <swann@devloup.dev>
2023-03-23 23:55:00 +0100

    start create cluster but issue with tokio threading

--- src/usecase/create_cluster.rs
+++ src/usecase/create_cluster.rs
@@ -1,6 +1,19 @@
 use actix_web::{post, HttpResponse, Responder};
+use postgresfixture::{runtime::Runtime, cluster::Cluster};
 
 #[post("/create-cluster")]
-pub async fn create_cluster(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+pub async fn create_cluster(_req_body: String) -> impl Responder {
+    for runtime in Runtime::find_on_path() {
+        println!("{:?}", runtime);
+        let cluster = Cluster::new("some/path", runtime);
+        cluster.start().unwrap();
+        let mut conn = cluster.connect("postgres").unwrap();
+        let result = conn.query("SHOW ALL", &[]).unwrap();
+        let params: std::collections::HashMap<String, String> = result
+            .into_iter()
+            .map(|row| (row.get::<usize, String>(0), row.get::<usize, String>(1)))
+            .collect();
+        println!("{:?}", params);
+    }
+    HttpResponse::Ok()
 }
