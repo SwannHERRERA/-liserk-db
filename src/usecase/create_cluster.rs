8ba408f193110d7be346098e81d745cf41863793
Swann HERRERA <swann@devloup.dev>
2023-03-25 15:09:19 +0100

    Try to use postgresfixture but does not work

--- src/usecase/create_cluster.rs
+++ src/usecase/create_cluster.rs
@@ -1,19 +1,27 @@
+use std::thread;
+
 use actix_web::{post, HttpResponse, Responder};
 use postgresfixture::{runtime::Runtime, cluster::Cluster};
 
+// Todo: Move postgres custom to postgres task
+
 #[post("/create-cluster")]
 pub async fn create_cluster(_req_body: String) -> impl Responder {
-    for runtime in Runtime::find_on_path() {
-        println!("{:?}", runtime);
-        let cluster = Cluster::new("some/path", runtime);
-        cluster.start().unwrap();
-        let mut conn = cluster.connect("postgres").unwrap();
-        let result = conn.query("SHOW ALL", &[]).unwrap();
-        let params: std::collections::HashMap<String, String> = result
-            .into_iter()
-            .map(|row| (row.get::<usize, String>(0), row.get::<usize, String>(1)))
-            .collect();
-        println!("{:?}", params);
-    }
+    let handle = thread::spawn(|| {
+        for runtime in Runtime::find_on_path() {
+            println!("{:?}", runtime);
+            let cluster = Cluster::new("some/path", runtime);
+            cluster.start().unwrap();
+            let mut conn = cluster.connect("postgres").unwrap();
+            let result = conn.query("SHOW ALL", &[]).unwrap();
+            let params: std::collections::HashMap<String, String> = result
+                .into_iter()
+                .map(|row| (row.get::<usize, String>(0), row.get::<usize, String>(1)))
+                .collect();
+            println!("{:?}", params);
+        }
+    });
+    // Todo: use the result
+    let _ = handle.join();
     HttpResponse::Ok()
 }
