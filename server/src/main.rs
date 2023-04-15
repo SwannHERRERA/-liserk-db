500879de3432cc131ddde322f478d77091741761
Swann HERRERA <swann.herrera@gmail.com>
2023-04-15 15:24:44 +0200

    fmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,9 @@
-use axum::{Server, Router};
+use axum::{Router, Server};
 
 #[tokio::main]
 async fn main() {
     // Build our application by creating our router.
-    let app = Router::new()
-        .route("/demo.json");
+    let app = Router::new().route("/demo.json");
 
     // Run our application as a hyper server on http://localhost:3000.
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
