d729aa975ce8dcb8e727f79a950ba0e58571799a
Swann HERRERA <swann.herrera@gmail.com>
2023-04-15 15:25:18 +0200

    remove route

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,7 @@ use axum::{Router, Server};
 #[tokio::main]
 async fn main() {
     // Build our application by creating our router.
-    let app = Router::new().route("/demo.json");
+    let app = Router::new();
 
     // Run our application as a hyper server on http://localhost:3000.
     Server::bind(&"0.0.0.0:3000".parse().unwrap())
